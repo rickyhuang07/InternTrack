bf15609a68de8f1adfd0df5e6d24220dbad7da31
Ricky Huang <ricky64623@gmail.com>
2026-09-17 00:06:54 -0500

    added delete confirmation

--- src/components/DeleteApplication.tsx
+++ src/components/DeleteApplication.tsx
@@ -1,5 +1,6 @@
 "use client"; // we need this bc we have to perform upon user interaction (click)
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function DeleteApplication({
   applicationId,
@@ -7,6 +8,7 @@ export default function DeleteApplication({
   applicationId: string;
 }) {
   const router = useRouter(); // gives client component access to next.js nav
+  const [showConfirmation, setShowConfirmation] = useState(false);
   async function handleDelete() {
     await fetch("/api/applications", {
       method: "DELETE",
@@ -21,12 +23,40 @@ export default function DeleteApplication({
   }
 
   return (
+    <>
     <button
       type="button"
-      onClick={handleDelete} // dont call delete until click detected
+      onClick={() => setShowConfirmation(true)} // delete confirmation popup
       className="rounded-lg bg-red-500 px-4 py-2 text-sm font-medium text-white transition hover:bg-red-600"
     >
       Delete Application
     </button>
+
+      {showConfirmation && ( // only render stuff inside when sc true
+      <div className="mt-3 rounded-lg border border-red-200 bg-red-50 p-4">
+        <p className="text-sm text-red-700">
+          Are you sure you want to delete this application?
+        </p>
+
+        <div className="mt-3 flex gap-2">
+          <button
+            type="button"
+            onClick={handleDelete}
+            className="rounded-lg bg-red-500 px-3 py-2 text-sm font-medium text-white"
+          >
+            Delete
+          </button>
+
+          <button
+            type="button"
+            onClick={() => setShowConfirmation(false)}
+            className="rounded-lg bg-gray-200 px-3 py-2 text-sm font-medium text-gray-700"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    )}
+  </>
   );
 }
